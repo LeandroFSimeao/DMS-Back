77479a97ba49396e6aca4371c672d6230a911c50
LeandroFSimeao <leandrosimeao@yahoo.com.br>
2023-07-09 19:04:08 -0300

    Https

--- DMS/Program.cs
+++ DMS/Program.cs
@@ -40,14 +40,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseCors();
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
